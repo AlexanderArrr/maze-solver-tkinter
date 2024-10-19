b46ab083be96863fb96f574f564dd7e82bba6533
AlexanderArrr <alexanderaretzger@gmail.com>
2024-10-19 18:39:48 +0200

    classes Point, Line, Cell added

--- graphics.py
+++ graphics.py
@@ -23,5 +23,44 @@ class Window:
     def close(self):
         self.is_running = False
 
+    def draw_line(self, line, fill_color):
+        line.draw(self.canvas, fill_color)
 
 
+class Point:
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
+
+class Line:
+    def __init__(self, p1, p2):
+        self.p1 = p1
+        self.p2 = p2
+    
+    def draw(self, canvas, fill_color):
+        canvas.create_line(self.p1.x, self.p1.y, self.p2.x, self.p2.y, fill=fill_color, width=2)
+
+class Cell:
+    def __init__(self, x1, y1, x2, y2, win):
+        self.x1 = x1
+        self.y1 = y1
+        self.x2 = x2
+        self.y2 = y2
+        self.win = win
+        
+        self.has_left_wall = True
+        self.has_top_wall = True
+        self.has_right_wall = True
+        self.has_bottom_wall = True
+
+    def draw(self):
+        if self.has_left_wall:
+            self.win.draw_line(Line(Point(self.x1, self.y1), Point(self.x1, self.y2)), "black")
+        if self.has_top_wall:
+            self.win.draw_line(Line(Point(self.x1, self.y1), Point(self.x2, self.y1)), "black")
+        if self.has_right_wall:
+            self.win.draw_line(Line(Point(self.x2, self.y1), Point(self.x2, self.y2)), "black")
+        if self.has_bottom_wall:
+            self.win.draw_line(Line(Point(self.x1, self.y2), Point(self.x2, self.y2)), "black")
+        
+        
--- main.py
+++ main.py
@@ -1,7 +1,15 @@
-from graphics import Window
+from graphics import Window, Line, Point, Cell
 
 def main():
     win = Window(800, 600)
+
+    # line = Line(Point(400, 400), Point(600,450))
+    # win.draw_line(line, "black")
+    cell = Cell(2, 2, 200, 200, win)
+    # cell.has_left_wall = False
+    # cell.has_top_wall = False
+    cell.draw()
+
     win.wait_for_close()
 
 main()
